6fa2ebc2496b1ba8191bef0d5465a98550aef481
AVA Sharkawy <dodoxzero@yahoo.com>
2022-11-16 03:06:16 +0200

    Update SmsRequest Params and SmsResponse to latest SMSMisr API Version

--- Elsheimy.Components.Sms.SmsMisr/SmsDetails.cs
+++ Elsheimy.Components.Sms.SmsMisr/SmsDetails.cs
@@ -5,7 +5,8 @@
     public string Username { get; set; }
     public string Password { get; set; }
     public int Language { get; set; }
-    public string Sender { get; set; }
+    public int Environment { get; set; }
+        public string Sender { get; set; }
     public string Mobile { get; set; }
     public string Message { get; set; }
     public string DelayUntil { get; set; }
